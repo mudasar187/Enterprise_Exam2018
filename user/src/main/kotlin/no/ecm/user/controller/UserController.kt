72f64e9d1220e6c9b266088d448512da5cc9c84e
Mudasar Ahmad <ahmmud16@hotmail.com>
2018-10-25 11:44:02 +0200

    added config for swagger ui for all modules

--- user/src/main/kotlin/no/ecm/user/controller/UserController.kt
+++ user/src/main/kotlin/no/ecm/user/controller/UserController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping(
         path = ["/"])
 @RestController
-class AdminController {
+class UserController {
 
     @GetMapping
     fun get(): ResponseEntity<String>? {
