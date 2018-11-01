733128e8c1497fcf07902ac6f61752155adc2649
Endre Synnes <synend16@student.westerdals.no>
2018-11-01 11:07:50 +0100

    working query resolver

--- creditcard/src/main/kotlin/no/ecm/creditcard/CreditCardApplication.kt
+++ creditcard/src/main/kotlin/no/ecm/creditcard/CreditCardApplication.kt
@@ -6,6 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication(scanBasePackages = ["no.ecm.creditcard"])
 class CreditCardApplication {}
 
+/*
+    UI accessible at
+    http://localhost:8080/graphiql
+    (note the "i" between graph and ql...)
+
+    UI graph representation at
+    http://localhost:8080/voyager
+
+ */
+
 fun main(args: Array<String>) {
     SpringApplication.run(CreditCardApplication::class.java, *args)
 }
