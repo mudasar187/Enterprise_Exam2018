40fe4cfaf43a2157c86eed12fecd250f8306579e
Mudasar Ahmad <ahmmud16@hotmail.com>
2018-11-02 12:34:55 +0100

    eureka server run and tested with running cinema, all works, need to clean dependencies

--- eureka/src/main/kotlin/no/ecm/eureka/EurukaServerApplication.kt
+++ eureka/src/main/kotlin/no/ecm/eureka/EurukaServerApplication.kt
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
-@SpringBootApplication(scanBasePackages = ["no.ecm.euruka"])
+@SpringBootApplication(scanBasePackages = ["no.ecm.eureka"])
 @EnableEurekaServer
-class EurekaServerApplication
+class EurekaServerApplication{}
 
 fun main(args: Array<String>) {
     SpringApplication.run(EurekaServerApplication::class.java, *args)
