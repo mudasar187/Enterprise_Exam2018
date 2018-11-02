a26a22ad14824b358f6c3d1c2fdd0a6622afc70c
Mudasar Ahmad <ahmmud16@hotmail.com>
2018-11-02 12:01:30 +0100

    setting up eureka, issues with import, testing on another machine

--- cinema/src/main/kotlin/no/ecm/cinema/CinemaApplication.kt
+++ cinema/src/main/kotlin/no/ecm/cinema/CinemaApplication.kt
@@ -3,8 +3,9 @@ package no.ecm.cinema
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
-//TODO import dto from utils module
+
 @SpringBootApplication(scanBasePackages = ["no.ecm.cinema"])
+
 class CinemaApplication {}
 
 fun main(args: Array<String>) {
--- eureka/src/main/kotlin/no/ecm/eureka/EurukaServerApplication.kt
+++ eureka/src/main/kotlin/no/ecm/eureka/EurukaServerApplication.kt
@@ -2,8 +2,10 @@ package no.ecm.eureka
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication(scanBasePackages = ["no.ecm.euruka"])
+@EnableEurekaServer
 class EurekaServerApplication
 
 fun main(args: Array<String>) {
