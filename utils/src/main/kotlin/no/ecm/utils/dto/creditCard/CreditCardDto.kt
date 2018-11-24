91fd095147f99b42d97e1a0e0fdd148ac398b270
MiniMarker <cfmarker@gmail.com>
2018-11-24 23:43:03 +0100

    there was a typo in the CreditCardDto that caused build failiure

--- utils/src/main/kotlin/no/ecm/utils/dto/creditCard/CreditCardDto.kt
+++ utils/src/main/kotlin/no/ecm/utils/dto/creditCard/CreditCardDto.kt
@@ -1,6 +1,6 @@
 package no.ecm.utils.dto.creditCard
 
-open class wCreditCardDto(
+open class CreditCardDto(
         var id: String? = null,
         cardNumber: String? = null,
         expirationDate: String? = null,
