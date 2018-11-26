f922c156da985d93e484ed924aa271d5be260228
MiniMarker <cfmarker@gmail.com>
2018-11-26 23:41:18 +0100

    added converting methods to dtoToEntityConverter

--- order/src/main/kotlin/no/ecm/order/model/converter/CouponConverter.kt
+++ order/src/main/kotlin/no/ecm/order/model/converter/CouponConverter.kt
@@ -1,9 +1,11 @@
 package no.ecm.order.model.converter
 
 import no.ecm.order.model.entity.Coupon
+import no.ecm.utils.converter.ConvertionHandler
 import no.ecm.utils.dto.order.CouponDto
 import no.ecm.utils.hal.PageDto
 import no.ecm.utils.response.CouponResponseDto
+import no.ecm.utils.validation.ValidationHandler
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import java.time.ZoneId
@@ -25,11 +27,17 @@ object CouponConverter {
 	
 	fun dtoToEntity(dto: CouponDto) : Coupon {
 		
+		val formattedTime = "${dto.expireAt!!}.000000"
+		val validatedTimeStamp: String = ValidationHandler.validateTimeFormat(formattedTime)
+		val parsedDateTime = ConvertionHandler.convertTimeStampToZonedTimeDate(validatedTimeStamp)
+		
+		/*
 		//Converting string to ZonedDateTime
 		val pattern = "yyyy-MM-dd HH:mm:ss.SSSSSS"
 		val parser: DateTimeFormatter = DateTimeFormatter.ofPattern(pattern).withZone(ZoneId.systemDefault())
 		
 		val parsedDateTime = ZonedDateTime.parse(dto.expireAt, parser) ?: null
+		*/
 		
 		return Coupon(
 			id = dto.id!!.toLong(),
