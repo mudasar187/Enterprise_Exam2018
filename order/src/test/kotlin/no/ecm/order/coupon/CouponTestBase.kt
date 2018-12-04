6d870062312a43b1e643941b8e32aa1e0614246e
MiniMarker <cfmarker@gmail.com>
2018-12-04 15:01:36 +0100

    written more tests

--- order/src/test/kotlin/no/ecm/order/coupon/CouponTestBase.kt
+++ order/src/test/kotlin/no/ecm/order/coupon/CouponTestBase.kt
@@ -91,4 +91,11 @@ abstract class CouponTestBase {
 		given().get().then().statusCode(200).body("data.list.size()", CoreMatchers.equalTo(size))
 	}
 	
+	fun getEtagFromId(id: String): String {
+		return RestAssured.given().accept(ContentType.JSON)
+			.get("/$id")
+			.then()
+			.extract().header("ETag")
+	}
+	
 }
