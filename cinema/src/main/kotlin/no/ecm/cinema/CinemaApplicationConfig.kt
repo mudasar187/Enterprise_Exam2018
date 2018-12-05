33d253168b13e7e5c07dacf2287bf580d6cef30a
Mudasar Ahmad <ahmmud16@hotmail.com>
2018-12-05 15:26:33 +0100

    removed hystrix from CinemaApplicationConfig

--- cinema/src/main/kotlin/no/ecm/cinema/CinemaApplicationConfig.kt
+++ cinema/src/main/kotlin/no/ecm/cinema/CinemaApplicationConfig.kt
@@ -16,19 +16,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 @EntityScan(basePackages = ["no.ecm.cinema"])
 class CinemaApplicationConfig {
 
-    init {
-        //Hystrix configuration
-        ConfigurationManager.getConfigInstance().apply {
-            // how long to wait before giving up a request?
-            setProperty("hystrix.command.default.execution.isolation.thread.timeoutInMilliseconds", 500) //default is 1000
-            // how many failures before activating the CB?
-            setProperty("hystrix.command.default.circuitBreaker.requestVolumeThreshold", 2) //default 20
-            setProperty("hystrix.command.default.circuitBreaker.errorThresholdPercentage", 2)
-            //for how long should the CB stop requests? after this, 1 single request will try to check if remote server is ok
-            setProperty("hystrix.command.default.circuitBreaker.sleepWindowInMilliseconds", 5000)
-        }
-    }
-
     @Bean
     fun swaggerApi(): Docket {
         return Docket(DocumentationType.SWAGGER_2)
