8987b743d6dcf5029d7df8019f1911a56648d0d5
MiniMarker <cfmarker@gmail.com>
2018-12-08 16:00:43 +0100

    refactored user

---
+++ user/src/main/kotlin/no/ecm/user/resolver/UserMutationResolver.kt
@@ -0,0 +1,125 @@
+package no.ecm.user.resolver
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver
+import graphql.execution.DataFetcherResult
+import graphql.servlet.GenericGraphQLError
+import no.ecm.user.model.converter.UserConverter
+import no.ecm.user.repository.UserRepository
+import no.ecm.utils.dto.user.UserDto
+import no.ecm.utils.logger
+import no.ecm.utils.messages.ExceptionMessages
+import no.ecm.utils.messages.InfoMessages
+import no.ecm.utils.messages.InfoMessages.Companion.entitySuccessfullyDeleted
+import org.springframework.stereotype.Component
+
+@Component
+class UserMutationResolver(
+	private var userRepository: UserRepository
+) : GraphQLMutationResolver {
+	
+	val logger = logger<UserMutationResolver>()
+	
+	fun createUser(input: UserDto): DataFetcherResult<String> {
+		
+		when {
+			input.username.isNullOrBlank() -> {
+				val errorMsg = ExceptionMessages.missingRequiredField("username")
+				logger.warn(errorMsg)
+				return DataFetcherResult<String>(null, listOf(GenericGraphQLError(errorMsg)))
+			}
+			input.dateOfBirth.isNullOrBlank() -> {
+				val errorMsg = ExceptionMessages.missingRequiredField("dateOfBirth")
+				logger.warn(errorMsg)
+				return DataFetcherResult<String>(null, listOf(GenericGraphQLError(errorMsg)))
+			}
+			input.email.isNullOrBlank() -> {
+				val errorMsg = ExceptionMessages.missingRequiredField("email")
+				logger.warn(errorMsg)
+				return DataFetcherResult<String>(null, listOf(GenericGraphQLError(errorMsg)))
+			}
+			input.name.isNullOrBlank() -> {
+				val errorMsg = ExceptionMessages.missingRequiredField("name")
+				logger.warn(errorMsg)
+				return DataFetcherResult<String>(null, listOf(GenericGraphQLError(errorMsg)))
+			}
+			
+			userRepository.existsById(input.username!!) -> {
+				val errorMsg = ExceptionMessages.resourceAlreadyExists("user", "username", input.username!!)
+				logger.warn(errorMsg)
+				return DataFetcherResult<String>(null, listOf(GenericGraphQLError(errorMsg)))
+			}
+			
+			else -> {
+				
+				val id = userRepository.save(UserConverter.dtoToEntity(input)).username.toString()
+				val msg = InfoMessages.entityCreatedSuccessfully("user", id)
+				logger.info(msg)
+				
+				return DataFetcherResult(id, listOf())
+			}
+		}
+	}
+	
+	fun deleteUserById(inputId: String): DataFetcherResult<String> {
+		
+		if (!userRepository.existsById(inputId)) {
+			val errorMsg = ExceptionMessages.notFoundMessage("User", "username", inputId)
+			logger.warn(errorMsg)
+			return DataFetcherResult<String>(null, listOf(GenericGraphQLError(errorMsg)))
+		}
+		
+		userRepository.deleteById(inputId)
+		
+		val msg = entitySuccessfullyDeleted("User", inputId)
+		logger.info(msg)
+		return DataFetcherResult(msg, listOf())
+	}
+	
+	fun updateUserById(userId: String?, name: String?, email: String?): DataFetcherResult<String> {
+		
+		when {
+			userId.isNullOrBlank() -> {
+				val errorMsg = ExceptionMessages.missingRequiredField("userId")
+				logger.warn(errorMsg)
+				return DataFetcherResult<String>(null, listOf(GenericGraphQLError(errorMsg)))
+			}
+			
+			name.isNullOrBlank() -> {
+				val errorMsg = ExceptionMessages.missingRequiredField("name")
+				logger.warn(errorMsg)
+				return DataFetcherResult<String>(null, listOf(GenericGraphQLError(errorMsg)))
+			}
+			
+			email.isNullOrBlank() -> {
+				val errorMsg = ExceptionMessages.missingRequiredField("email")
+				logger.warn(errorMsg)
+				return DataFetcherResult<String>(null, listOf(GenericGraphQLError(errorMsg)))
+			}
+			
+			!userRepository.existsById(userId!!) -> {
+				val errorMsg = ExceptionMessages.notFoundMessage("User", "username", userId!!)
+				logger.warn(errorMsg)
+				return DataFetcherResult<String>(null, listOf(GenericGraphQLError(errorMsg)))
+			}
+			
+			else -> {
+				
+				val user = userRepository.findById(userId).get()
+				
+				if (user.username!! != userId) {
+					val errorMsg = ExceptionMessages.subIdNotMatchingParentId(user.username!!, userId!!)
+					logger.warn(errorMsg)
+					return DataFetcherResult<String>(null, listOf(GenericGraphQLError(errorMsg)))
+				}
+				
+				user.name = name!!
+				user.email = email!!
+				
+				userRepository.save(user)
+				val msg = InfoMessages.entitySuccessfullyUpdated("user", userId)
+				logger.info(msg)
+				return DataFetcherResult(msg, listOf())
+			}
+		}
+	}
+}
---
+++ user/src/test/kotlin/no/ecm/user/UserTest.kt
@@ -0,0 +1,259 @@
+package no.ecm.user
+
+import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
+import org.hamcrest.CoreMatchers
+import org.hamcrest.Matchers.equalTo
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+
+
+class UserTest : TestBase() {
+	
+	
+	@Test
+	fun testCreateAndGetById() {
+		
+		val username = "frodo55"
+		val email = "frodo@shire.no"
+		val name = "Frodo Baggins"
+		val dateOfBirth = "2018-11-04"
+		
+		val usernameRes = createUser(username, dateOfBirth, name, email)
+		
+		assertNotNull(usernameRes)
+		assertEquals(username, usernameRes)
+		
+		getUserByUsername(username)!!
+			.statusCode(200)
+			
+			// all queries params are present
+			.body("data.userById.size()", equalTo(4))
+			
+			// check individual params
+			.body("data.userById.username", equalTo(username))
+			.body("data.userById.email", equalTo(email))
+			.body("data.userById.name", equalTo(name))
+			.body("data.userById.dateOfBirth", equalTo(dateOfBirth))
+	}
+	
+	@Test
+	fun testCreateUserWithMissingArguments() {
+		
+		val dateOfBirth = "2018-03-03"
+		
+		val createQuery = """
+                    { "query" :
+                         "mutation{createUser(user:{dateOfBirth:\"$dateOfBirth\"})}"
+                    }
+                    """.trimIndent()
+		
+		given()
+			.accept(ContentType.JSON)
+			.contentType(ContentType.JSON)
+			.body(createQuery)
+			.post()
+			.then()
+			.statusCode(200)
+			.body("data.createUser", CoreMatchers.nullValue())
+			.body("errors.message[0]", CoreMatchers.notNullValue())
+	}
+	
+	@Test
+	fun testCreateUserWithInvalidData() {
+		val username = "frodobaggins"
+		val email = "frodo@shire.no"
+		val name = "Frodo Baggins"
+		val dateOfBirth = "2018-11-04"
+		
+		val noUsername = """
+                    { "query" :
+                         "mutation{createUser(user:{username:null,dateOfBirth:\"$dateOfBirth\",name:\"$name\",email:\"$email\"})}"
+                    }
+                    """.trimIndent()
+		
+		val noDateOfBirth = """
+                    { "query" :
+                         "mutation{createUser(user:{username:\"$username\",dateOfBirth:null,name:\"$name\",email:\"$email\"})}"
+                    }
+                    """.trimIndent()
+		
+		val noName = """
+                    { "query" :
+                         "mutation{createUser(user:{username:\"$username\",dateOfBirth:\"$dateOfBirth\",name:null,email:\"$email\"})}"
+                    }
+                    """.trimIndent()
+		
+		val noEmail = """
+                    { "query" :
+                         "mutation{createUser(user:{username:\"$username\",dateOfBirth:\"$dateOfBirth\",name:\"$name\",email:null})}"
+                    }
+                    """.trimIndent()
+		
+		invalidUserQuery(noUsername)!!
+			.body("data.createUser", CoreMatchers.nullValue())
+			.body("errors.message[0]", CoreMatchers.notNullValue())
+		
+		invalidUserQuery(noDateOfBirth)!!
+			.body("data.createUser", CoreMatchers.nullValue())
+			.body("errors.message[0]", CoreMatchers.notNullValue())
+		
+		invalidUserQuery(noName)!!
+			.body("data.createUser", CoreMatchers.nullValue())
+			.body("errors.message[0]", CoreMatchers.notNullValue())
+		
+		invalidUserQuery(noEmail)!!
+			.body("data.createUser", CoreMatchers.nullValue())
+			.body("errors.message[0]", CoreMatchers.notNullValue())
+		
+	}
+	
+	
+	@Test
+	fun testGetNonExistingUser() {
+		
+		getUserByUsername("arhurdent42")!!
+			.statusCode(200)
+			.body("data.userById", equalTo(null))
+	}
+	
+	@Test
+	fun testDeleteUser() {
+		
+		val username = "frodo55"
+		val email = "frodo@shire.no"
+		val name = "Frodo Baggins"
+		val dateOfBirth = "2018-11-04"
+		
+		val usernameRes = createUser(username, dateOfBirth, name, email)
+		
+		assertNotNull(usernameRes)
+		assertEquals(username, usernameRes)
+		
+		getUserByUsername(username)!!
+			.statusCode(200)
+		
+		val deleteQuery = """
+                    { "query" :
+                         "mutation{deleteUserById(id:\"$username\")}"
+                    }
+                    """.trimIndent()
+		
+		given()
+			.accept(ContentType.JSON)
+			.contentType(ContentType.JSON)
+			.body(deleteQuery)
+			.post()
+			.then()
+			.statusCode(200)
+			.body("data.deleteUserById", CoreMatchers.notNullValue())
+			.body("errors.message", CoreMatchers.nullValue())
+	}
+	
+	@Test
+	fun testDeleteNonExistingUser() {
+		
+		val deleteQuery = """
+                    { "query" :
+                         "mutation{deleteUserById(id: \"12346787654\")}"
+                    }
+                    """.trimIndent()
+		
+		given()
+			.accept(ContentType.JSON)
+			.contentType(ContentType.JSON)
+			.body(deleteQuery)
+			.post()
+			.then()
+			.statusCode(200)
+			.body("data.deleteUserById", CoreMatchers.nullValue())
+			.body("errors.message", CoreMatchers.notNullValue())
+		
+	}
+	
+	@Test
+	fun testUpdateUser() {
+		val username = "frodo55"
+		val email = "frodo@shire.no"
+		val name = "Frodo Baggins"
+		val dateOfBirth = "2018-11-04"
+		
+		val usernameRes = createUser(username, dateOfBirth, name, email)
+		
+		assertNotNull(usernameRes)
+		assertEquals(username, usernameRes)
+		
+		getUserByUsername(username)!!
+			.statusCode(200)
+		
+		//UPDATE
+		val updatedEmail = "bilbo@shire.no"
+		val updatedName = "Bilbo Baggins"
+		
+		
+		val updateQuery = """
+                    { "query" :
+                         "mutation{updateUserById(id:\"$username\", name:\"$updatedName\", email:\"$updatedEmail\")}"
+                    }
+                    """.trimIndent()
+		
+		invalidUserQuery(updateQuery)!!
+			.body("data.updateUserById", CoreMatchers.notNullValue())
+		
+		getUserByUsername(username)!!
+			.statusCode(200)
+			.body("data.userById.username", equalTo(username))
+			.body("data.userById.email", equalTo(updatedEmail))
+			.body("data.userById.name", equalTo(updatedName))
+			.body("data.userById.dateOfBirth", equalTo(dateOfBirth))
+	}
+	
+	@Test
+	fun testUpdateUserWithInvalidData() {
+		
+		val username = "frodo55"
+		val email = "frodo@shire.no"
+		val name = "Frodo Baggins"
+		val dateOfBirth = "2018-11-04"
+		
+		val usernameRes = createUser(username, dateOfBirth, name, email)
+		
+		val updatedEmail = "bilbo@shire.no"
+		val updatedName = "Bilbo Baggins"
+		
+		val noUsername = """
+                    { "query" :
+                         "mutation{updateUserById(id: null, name:\"$updatedName\", email:\"$updatedEmail\")}"
+                    }
+                    """.trimIndent()
+		
+		val noName = """
+                    { "query" :
+                         "mutation{updateUserById(id:\"$usernameRes\", name:null, email:\"$updatedEmail\")}"
+                    }
+                    """.trimIndent()
+		
+		val noEmail = """
+                    { "query" :
+                         "mutation{updateUserById(id:\"$usernameRes\", name:\"$updatedName\", email:null)}"
+                    }
+                    """.trimIndent()
+		
+		invalidUserQuery(noUsername)!!
+			.body("data.updateUserById", CoreMatchers.nullValue())
+			.body("errors.message[0]", CoreMatchers.notNullValue())
+			.extract().body().jsonPath().prettyPrint()
+		
+		invalidUserQuery(noName)!!
+			.body("data.updateUserById", CoreMatchers.nullValue())
+			.body("errors.message[0]", CoreMatchers.notNullValue())
+			.extract().body().jsonPath().prettyPrint()
+		
+		invalidUserQuery(noEmail)!!
+			.body("data.updateUserById", CoreMatchers.nullValue())
+			.body("errors.message[0]", CoreMatchers.notNullValue())
+			.extract().body().jsonPath().prettyPrint()
+		
+	}
+}
