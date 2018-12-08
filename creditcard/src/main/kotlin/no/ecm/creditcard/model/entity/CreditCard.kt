0c055af300bfa134d0e5e8d3dacb4e44a0f27938
Mudasar Ahmad <ahmmud16@hotmail.com>
2018-12-08 19:51:00 +0100

    partical clean up

--- creditcard/src/main/kotlin/no/ecm/creditcard/model/entity/CreditCard.kt
+++ creditcard/src/main/kotlin/no/ecm/creditcard/model/entity/CreditCard.kt
@@ -1,6 +1,5 @@
 package no.ecm.creditcard.model.entity
 
-import org.springframework.format.annotation.NumberFormat
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
