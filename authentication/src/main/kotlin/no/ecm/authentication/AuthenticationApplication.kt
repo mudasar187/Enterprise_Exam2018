98e135cd65b143d1873de98bbec6b5bc392575d7
Mudasar Ahmad <ahmmud16@hotmail.com>
2018-12-13 12:39:41 +0100

    handle required fields added for auth

--- authentication/src/main/kotlin/no/ecm/authentication/AuthenticationApplication.kt
+++ authentication/src/main/kotlin/no/ecm/authentication/AuthenticationApplication.kt
@@ -1,5 +1,6 @@
 package no.ecm.authentication
 
+import no.ecm.utils.exception.RestResponseEntityExceptionHandler
 import org.springframework.amqp.core.DirectExchange
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -7,8 +8,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.context.annotation.Bean
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.web.bind.annotation.ControllerAdvice
 
-@SpringBootApplication(scanBasePackages = ["no.ecm.authentication"])
+@SpringBootApplication(scanBasePackages = ["no.ecm.authentication", "no.ecm.utils.exception"])
+@ControllerAdvice(basePackageClasses = [RestResponseEntityExceptionHandler::class])
 @EnableEurekaClient
 class AuthenticationApplication{
 
