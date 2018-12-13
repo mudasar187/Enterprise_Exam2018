c9aa94f0681adcc5105e24608cb86c731c84401f
MiniMarker <cfmarker@gmail.com>
2018-12-13 10:02:27 +0100

    added comments for gateway problems

--- gateway/src/main/kotlin/no/ecm/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/no/ecm/gateway/GatewayApplication.kt
@@ -12,6 +12,14 @@ import java.util.*
 @SpringBootApplication(scanBasePackages = ["no.ecm.gateway"])
 @EnableDiscoveryClient
 class GatewayApplication{
+    
+    // Gateway seems to strip requests for headers. This is a known problem and there has been creates a Issue on github for this:
+    // https://github.com/spring-cloud/spring-cloud-gateway/issues/112
+    
+    // solution for for this is found on this issue comment:
+    // https://github.com/spring-cloud/spring-cloud-gateway/issues/229#issuecomment-394132177
+    // there is also need for a change in application.yml file, see this file for comments and link
+    
     @Bean
     fun corsConfiguration(routePredicateHandlerMapping: RoutePredicateHandlerMapping): CorsConfiguration {
         val corsConfiguration = CorsConfiguration().applyPermitDefaultValues()
